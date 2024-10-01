fb413de9e9d889e21745b5755caa584ac12fa052
Nikita1814 <kravchenko1814@gmail.com>
2024-10-02 00:45:24 +0300

    feat: implement rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,28 @@
+import {access} from "node:fs";
+import {rename as fsRename} from "node:fs" 
+
 const rename = async () => {
-    // Write your code here 
+    // checking if the file is missing
+    await access(
+        'src/fs/files/wrongFilename.txt', 
+        (err) => {
+            if (err) {
+                throw new Error ("FS operation failed")
+            } 
+        }
+    )
+    //checking if the renamed file already exists
+    await access(
+        'src/fs/files/properFilename.md', 
+        async (err) => {
+            if (err) {
+                await fsRename('src/fs/files/wrongFilename.txt', 'src/fs/files/properFilename.md', () => {})
+            } else {
+                throw new Error ("FS operation failed")
+            }
+        }
+    )
+
 };
 
 await rename();
