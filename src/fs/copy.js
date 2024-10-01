e6ef458d5d18c6c92e38a1819de4f662adb00318
Nikita1814 <kravchenko1814@gmail.com>
2024-10-02 00:09:28 +0300

    feat: implement coppy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,31 @@
+import {access, cp} from "node:fs"
 const copy = async () => {
-    // Write your code here 
+    // checkinng that files folder exists 
+    await access(
+        'src/fs/files', 
+        (err) => {
+            if (err) {
+                throw new Error ("files directory does not exist")
+            }
+        }
+    )
+    // checking if copy exists and creating it in case it does not
+    await access (
+        'src/fs/files_copy',
+        async (err) => {
+            if (err) {
+                await cp(
+                    'src/fs/files', 
+                    'src/fs/files_copy', 
+                    {recursive: true}, 
+                    (err) => {}
+                )  
+            } else {
+                throw new Error("FS operation failed")  
+            }
+        }
+    )
+
 };
 
 await copy();
