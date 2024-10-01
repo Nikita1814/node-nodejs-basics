b4435b8a51b16e8e375ab2813c9a8a02d797e3d5
Nikita1814 <kravchenko1814@gmail.com>
2024-10-02 01:28:32 +0300

    feat: implement delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,17 @@
+import {access, rm} from 'node:fs'
+
 const remove = async () => {
     // Write your code here 
+    await access(
+        'src/fs/files/fileToRemove.txt', 
+        async (err) => {
+            if (err) {
+                throw new Error ("FS operation failed")
+            } else {
+                await rm('src/fs/files/fileToRemove.txt', (err) => {})
+            }
+        }
+    )
 };
 
 await remove();
