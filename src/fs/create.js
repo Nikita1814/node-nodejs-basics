306555430ac776bc930ad86b32a11713a8b01403
Nikita1814 <kravchenko1814@gmail.com>
2024-10-01 23:39:22 +0300

    feat: implement create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,17 @@
+import {writeFile, access} from "node:fs"
+
 const create = async () => {
-    // Write your code here 
+    const text = "I am fresh and young";
+    await access(
+        'src/fs/files/fresh.txt', 
+        async (err) => {
+            if (err) {
+                await writeFile('src/fs/files/fresh.txt', text, (err) => {})
+            } else {
+                throw new Error('FS operation failed');
+             }
+        }
+    );
 };
 
 await create();
