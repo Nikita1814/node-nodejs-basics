777e2ad23758efb706fb4a19ba0cd9d60c432ed5
Nikita1814 <kravchenko1814@gmail.com>
2024-10-03 00:35:44 +0300

    feat: implement args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,41 @@
+import process from 'node:process';
+
 const parseArgs = () => {
-    // Write your code here 
+    const propNameStirngMatch = /^--.*$/
+    const props =  Object.values(process.argv)
+    const propsToDisplay = {}
+    props.map( (variable, idx) => {
+
+    if (
+        propNameStirngMatch.test(variable) 
+        && !(propNameStirngMatch.test(props[idx-1]))
+    ){
+        propsToDisplay[variable] = 'prop'
+    } else if (
+        propNameStirngMatch.test(variable) 
+        && props[idx-1] 
+        && propNameStirngMatch.test(props[idx-1]) 
+        && propsToDisplay[props[idx-1]]
+    ) {
+        propsToDisplay[props[idx-1]] = variable
+    } else if (
+        propNameStirngMatch.test(variable) 
+        && props[idx-1] 
+        && propNameStirngMatch.test(props[idx-1])
+        && !propNameStirngMatch.test(props[idx+1])  
+    ) {
+        propsToDisplay[variable] = 'prop'
+    } else if (
+        !propNameStirngMatch.test(variable) 
+        && propNameStirngMatch.test(props[idx-1]) 
+        &&  propsToDisplay[props[idx-1]] 
+    ) {
+        propsToDisplay[props[idx-1]] = variable
+    }
+    })
+    Object.entries(propsToDisplay).map((entry => {
+        console.log(`${entry[0]} is ${entry[1]}`)
+    }))
 };
 
 parseArgs();
