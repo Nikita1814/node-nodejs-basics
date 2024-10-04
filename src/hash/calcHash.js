13e961d6692afd801daf05f16fe97617fa55eb85
Nikita1814 <kravchenko1814@gmail.com>
2024-10-05 00:20:42 +0300

    feat: add hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,15 @@
+import {createHash} from "node:crypto"
+import { createReadStream } from "node:fs";
+
 const calculateHash = async () => {
-    // Write your code here 
+    const hash = createHash('sha256')
+    const readStream = createReadStream('src/hash/files/fileToCalculateHashFor.txt')
+    readStream.on('readable', () => {
+        const data = readStream.read()
+        if(data) {
+            console.log(hash.update(data).digest('hex'));
+        }
+    })
 };
 
 await calculateHash();
