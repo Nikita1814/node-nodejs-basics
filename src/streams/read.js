f7bc801ce10593458bf7260e5bb893037ec1c0d8
Nikita1814 <kravchenko1814@gmail.com>
2024-10-06 00:07:49 +0300

    feat: implement read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,12 @@
+import { createReadStream } from "node:fs";
+
 const read = async () => {
-    // Write your code here 
+    const readStream = await createReadStream('src/streams/files/fileToRead.txt')
+    readStream.on('data', (data) => {
+        if (data) {
+            process.stdout.write(data)
+        }
+    })
 };
 
 await read();
