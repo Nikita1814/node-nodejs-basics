cb3d5ae6173fa090ed36d2e087b2bb9b75fd925c
Nikita1814 <kravchenko1814@gmail.com>
2024-10-07 00:26:38 +0300

    fat: implement main and worker

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,27 @@
+import { Worker } from 'node:worker_threads';
+import { cpus } from 'node:os';
+
 const performCalculations = async () => {
-    // Write your code here
+   const cores = cpus().length;
+   const res = [];
+    for(let i = 0; i < cores; i++ ) {
+        const processPromise =  new Promise((resolve, reject) => {
+            const worker = new Worker('./src/wt/worker.js',{workerData: {num: 10 + i}})
+
+            worker.on("message", (result) => {
+                  resolve({ status: "resolved", result: result });
+            });
+        
+            worker.on("error", (msg) => {
+                    resolve({status: "error", result: null});
+            });            
+        });
+
+        res.push(processPromise);
+    }
+
+    const finalRes = await Promise.all(res);
+    console.log(finalRes);
 };
 
 await performCalculations();
