0ba90ca2dc0533a381dbde9927cfe7bcca549fd1
Nikita1814 <kravchenko1814@gmail.com>
2024-10-06 18:24:21 +0300

    feat: implement transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,12 @@
+import { Transform } from 'node:stream'
+
 const transform = async () => {
-    // Write your code here 
+    const reverseTransform = () => new Transform ({
+        transform(data, encoding, callback) {
+            callback(null, data.toString().split('').reverse().join(''));
+        }
+    })
+    process.stdin.pipe(reverseTransform()).pipe(process.stdout);
 };
 
-await transform();
+await transform()
