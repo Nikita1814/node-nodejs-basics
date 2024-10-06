9fcf233669f02c295517562f0e84fba2a1f7e588
Nikita1814 <kravchenko1814@gmail.com>
2024-10-06 18:53:34 +0300

    feat: implement compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,13 @@
+import { createReadStream, createWriteStream } from  "node:fs";
+import { createGzip } from "node:zlib";
+
 const compress = async () => {
-    // Write your code here 
+    const zip = createGzip();
+    const source = createReadStream('src/zip/files/fileToCompress.txt')
+    const result = createWriteStream('src/zip/files/archive.gz')
+
+    source.pipe(zip).pipe(result)
+
 };
 
 await compress();
