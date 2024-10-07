ffa98f4d3c26aab9138794f0c159efb3701f70a8
Nikita1814 <kravchenko1814@gmail.com>
2024-10-07 23:26:51 +0300

    feat: implement child process

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,14 @@
+import {spawn, fork} from 'node:child_process';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const spawnedProcess = spawn('node', ['./src/cp/files/script.js', ...args]);
+    
+    process.stdin.pipe(spawnedProcess.stdin)
+    spawnedProcess.stdout.on("data", (data) => {
+        console.log(data.toString())
+    })
+
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['Hello', 'is', 'it', 'me', 'you', 'are', 'looking', 'for']);
